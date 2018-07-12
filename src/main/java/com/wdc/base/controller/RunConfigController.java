8d1190c8f27464882a3366c9f07b51ad244c29fb
sc1000255563 <saikatsakura@gmail.com>
2018-07-12 16:50:25 +0530

    added method to insert record in h2 persistable database

--- src/main/java/com/wdc/base/controller/RunConfigController.java
+++ src/main/java/com/wdc/base/controller/RunConfigController.java
@@ -1,9 +1,12 @@
 package com.wdc.base.controller;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.wdc.base.repository.entity.RunConfiguration;
 import com.wdc.base.service.RunConfigurationService;
@@ -11,16 +14,21 @@ import com.wdc.base.service.RunConfigurationService;
 @Controller
 public class RunConfigController {
 	
+	private static final Logger logger = Logger.getLogger(RunConfigController.class.getName());
+	
 	@Autowired
 	private RunConfigurationService service;
 	
 	
 	@GetMapping(path = "/rc/landing")
 	public String landingPage(Model model) {
+		
 		return "rclanding";
 	}
 	
+	@PostMapping(path="/rc/add")
 	public String add(RunConfiguration configuration) {
+		logger.info("############ inside add");
 		service.add(configuration);
 		return "redirect:/rc/landing";
 	}
